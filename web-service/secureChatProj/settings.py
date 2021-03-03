0bcb7555fb9f64c75d9c13f9878567e27133aa25
intadd <intadd@naver.com>
2021-03-03 04:04:43 -0800

    dockerlize

--- web-service/secureChatProj/settings.py
+++ web-service/secureChatProj/settings.py
@@ -135,3 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+print(BASE_DIR)
+print(ROOT_DIR)
+STATIC_ROOT = os.path.join(ROOT_DIR,'static')
